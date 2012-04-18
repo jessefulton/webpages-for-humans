547fcd9c5859cb6a82d76e4e358002b9c5ec3de2
Jesse Fulton <jessefulton+github@gmail.com>
2012-04-17 21:08:05 -0700

    bookmarklet

--- app.js
+++ app.js
@@ -61,7 +61,12 @@ app.helpers({
 		, "isNumber": function(num) {
 			return (typeof(num) == "number") && !isNaN(num);
 		}
-		, "bookmarklet": 'javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.7.1",function($,L){var jsCode = document.createElement("script");jsCode.setAttribute("src", "https://raw.github.com/jessefulton/webpages-for-humans/master/public/javascripts/bookmarklet.js");document.body.appendChild(jsCode);});'
+		, "bookmarklet": '(function(){var d=document.getElementsByTagName("body")[0],e=false;if(typeof jQuery!="undefined"){a();return}else{if(typeof $=="function"){e=true}}function c(h,j){var g=document.createElement("script");g.src=h;var i=document.getElementsByTagName("head")[0],b=false;g.onload=g.onreadystatechange=function(){if(!b&&(!this.readyState||this.readyState=="loaded"||this.readyState=="complete")){b=true;j();g.onload=g.onreadystatechange=null;i.removeChild(g)}};i.appendChild(g)}c("https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js",function(){if(typeof jQuery=="undefined"){}else{msg="This page is now jQuerified with v"+jQuery.fn.jquery;if(e){msg+=" and noConflict(). Use $jq(), not $()."}}return f()});function a(){var b=document.createElement("script");b.setAttribute("src","https://raw.github.com/jessefulton/webpages-for-humans/master/public/javascripts/bookmarklet.js");document.body.appendChild(b)}function f(){window.setTimeout(function(){if(typeof jQuery=="undefined"){}else{if(e){$jq=jQuery.noConflict()}a()}},2500)}})();'
+		
+		
+			//'javascript:(function(){var doIt=function(){var a=document.createElement("script");a.setAttribute("src","https://raw.github.com/jessefulton/webpages-for-humans/master/public/javascripts/bookmarklet.js");document.body.appendChild(a)};if(typeof(jQuery)=="undefined"){var s=document.createElement("script");s.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js");s.onload=doIt();document.getElementsByTagName("body")[0].appendChild(s)}else{doIt()};})();'
+		
+		//'javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.7.1",function($,L){var jsCode = document.createElement("script");jsCode.setAttribute("src", "https://raw.github.com/jessefulton/webpages-for-humans/master/public/javascripts/bookmarklet.js");document.body.appendChild(jsCode);});'
 		//via http://beardscratchers.com/journal/using-javascript-to-get-the-hostname-of-a-url
 		, "hostname": function(str) {
 			var re = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im');
@@ -80,3 +85,73 @@ app.listen(port, function () {
 	console.log('    app listening on ' + app.set("basedomain"));
     console.log('    NODE_ENV = ' + process.env.NODE_ENV);
 });
+
+
+
+
+
+
+
+
+/*************
+BOOKMARKLET SCRIPT
+
+
+(function() {
+  var b=document.getElementsByTagName('body')[0]
+	, otherlib = false;
+  if(typeof jQuery!='undefined') {
+    doIt();
+	return
+  } else if (typeof $=='function') {
+    otherlib=true;
+  }
+ 
+  // more or less stolen form jquery core and adapted by paul irish
+  function getScript(url,success){
+    var script=document.createElement('script');
+    script.src=url;
+    var head=document.getElementsByTagName('head')[0],
+        done=false;
+    // Attach handlers for all browsers
+    script.onload=script.onreadystatechange = function(){
+      if ( !done && (!this.readyState
+           || this.readyState == 'loaded'
+           || this.readyState == 'complete') ) {
+        done=true;
+        success();
+        script.onload = script.onreadystatechange = null;
+        head.removeChild(script);
+      }
+    };
+    head.appendChild(script);
+  }
+  getScript("https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js",function() {
+    if (typeof jQuery=='undefined') {
+		//error
+    } else {
+      msg='This page is now jQuerified with v' + jQuery.fn.jquery;
+      if (otherlib) {msg+=' and noConflict(). Use $jq(), not $().';}
+    }
+    return showMsg();
+  });
+  function doIt() {
+	var jsCode = document.createElement("script");
+	jsCode.setAttribute("src", "https://raw.github.com/jessefulton/webpages-for-humans/master/public/javascripts/bookmarklet.js");
+	document.body.appendChild(jsCode);
+  }
+  function showMsg() {
+    window.setTimeout(function() {
+      if (typeof jQuery=='undefined') {
+
+      } else {
+        if (otherlib) {
+          $jq=jQuery.noConflict();
+        }
+		doIt();
+      }
+    } ,2500);    
+  }
+})();
+
+*****************/
